0da17fdc9e7d21e978fba7ef0a09c18597ce5376
oennadou <oennadou42@gmail.com>
2024-11-16 05:36:04 -0800

    00

--- src/App.js
+++ src/App.js
@@ -3,146 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className='oussama'>
-        <div>logo</div>
-        <div className='center-header'>
-          <div>home</div>
-          <div>about</div>
-          <div>blog</div>
-        </div>
-        <div className='right-header'>
-          <div>search</div>
-          <button>bolo</button>
-        </div>
-      </header>
-      <main>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-        <div> this is my main  </div>
-      </main>
-      <footer>
-        this is my footer
-      </footer>
-    </div>
+     <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="/logo.png" alt="MetaBlog Logo" />
+        <span class="bold-text">info-tasjil </span>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#blog">Blog</a></li>
+        <li><a href="#single-post">Single Post</a></li>
+        <li><a href="#pages">Pages</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="navbar-search">
+        <input type="text" placeholder="Search" />
+        <button>
+          <i className="fa fa-search"></i>
+        </button>
+      </div>
+      <button className="toggle-theme">
+        <i className="fa fa-sun-o"></i>
+      </button>
+    </nav>
   );
 }
 
